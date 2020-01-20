1be0dff6c5cf1c424d2118c7fac159238846a0a8
maksym-rakomin <49858760+maksym-rakomin@users.noreply.github.com>
2020-01-20 14:11:43 +0200

    Изменена функция, теперь результат выводится в докумени

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 ;(function(){
   
-    let arr = [];
+    let arr = [],
+    promptValue = null,
+    result = null,
+    enterList = document.querySelector(".enter__list");
+    compList = document.querySelector(".comp__list"),
 
-    alert('Здравствуйте! Введите значение, а функция покажет это число или текст');
-
-    let promptValue;
+    console.log(compList);
+    
+    promptValue = alert('Здравствуйте! Введите значение, а функция покажет это число или текст');
     
     do {
         promptValue = prompt("Ведите значние: число или текстовое значнеие (буква, слово или предложение)", '=)');
         if (promptValue != null){arr.push(promptValue)};
     } while (promptValue)
+    
+    result = map(whatIsIt, arr);
+
+    pastList(arr, enterList);
+    pastList(result, compList);
 
     function whatIsIt(val) {
         if (isNaN(val)){
@@ -17,18 +26,22 @@
         } else {
             return ` ${val} - число`;
         }
-    }
-
+    };
+    
     function map(fun, array) {
         let newArray = [];
-
+        
         array.forEach(element => newArray.push(fun(element)));
         return newArray;
+    };
+
+    function pastList(arr, elem) {
+        arr.forEach(element => {
+
+            let li = document.createElement("li");
+            li.innerHTML = `<li>${element}</li>`;
+            elem.append(li);
+        });
     }
-
-    let result = map(whatIsIt, arr);
-
-    console.log(`Это введенный массив: ${arr} `);
-    console.log(`А этот массив результирующий: ${result} `);
 
 })();
